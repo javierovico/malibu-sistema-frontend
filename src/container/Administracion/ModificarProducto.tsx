91ec549a87c17426b88b0dd1a16a3733782d7e44
javierovico <javierovico@gmail.com>
2022-06-22 10:04:17 -0400

    Antes de cambiar a withFormik para recargar defaultValue

--- src/container/Administracion/ModificarProducto.tsx
+++ src/container/Administracion/ModificarProducto.tsx
@@ -1,19 +1,43 @@
-import {useEffect, useState} from "react";
-import {IProducto} from "../../modelos/Producto";
+import React, {useEffect, useMemo, useState} from "react";
+import {IProducto, productoVacio, URL_GET_PRODUCTOS} from "../../modelos/Producto";
 import VistaError from "../../components/UI/VistaError";
+import {errorRandomToIError, IError} from "../../modelos/ErrorModel";
+import axios from "axios";
+import ResponseAPI from "../../modelos/ResponseAPI";
+import {Divider} from "antd";
+import {Formik, Form, Field} from "formik";
 
 interface ArgumentosModificarProducto {
     productoId?: number,        //si esta definido, es el producto a editar
 }
 
 function useProducto(productoId: number|undefined) {
-    const [producto, setProducto] = useState<IProducto|undefined>()
+    const [producto, setProducto] = useState<IProducto>(productoVacio)
     const [isProductoLoading, setIsProductoLoading] = useState<boolean>(true)
-    const [errorProducto, setErrorProducto] = useState<string|undefined>("ERROR");
+    const [errorProducto, setErrorProducto] = useState<IError|undefined>(undefined);
+    const vistaError = useMemo(()=>errorProducto?<VistaError error={errorProducto}/>:undefined,[errorProducto])
+    useEffect(()=>{
+        setIsProductoLoading(true)
+        setProducto(productoVacio)
+        setErrorProducto(undefined)
+        axios.get<ResponseAPI<{producto:IProducto}>>(`${URL_GET_PRODUCTOS}/${productoId}`,{
+            params:{
+
+            }
+        })
+            .then(({data}) => {
+                setProducto(data.data.producto)
+            })
+            .catch(e=> {
+                setErrorProducto(errorRandomToIError(e))
+            })
+            .finally(()=>setIsProductoLoading(false))
+
+    },[productoId])
     return {
         producto,
         isProductoLoading,
-        errorProducto
+        vistaError
     }
 }
 
@@ -24,13 +48,23 @@ export default function ModificarProducto (arg: ArgumentosModificarProducto) {
     const {
         producto,
         isProductoLoading,
-        errorProducto
+        vistaError
     } = useProducto(productoId)
     useEffect(()=>{
         console.log({producto})
     },[producto])
+    const formikForm = useMemo(()=>(<Formik
+        initialValues={producto}
+        onSubmit={(e) => { console.log(e)}}
+    >
+        {(formikProp)=>{
+            console.log(formikProp.values.nombre)
+            return <></>
+        }}
+    </Formik>),[producto])
     const vistaNormal = <>
-        <p> Modificacion de producto</p>
+        <Divider>Modificacion de producto</Divider>
+        {formikForm}
     </>
-    return errorProducto ? <></> : vistaNormal
+    return vistaError ? vistaError : vistaNormal
 }
