26dbae194ca8af7a6c89e6db9262e2f7e4639918
javierovico <javierovico@gmail.com>
2022-07-01 14:39:06 -0400

    antes de reutlizar existeEnArray

--- src/modelos/Generico.tsx
+++ src/modelos/Generico.tsx
@@ -5,7 +5,18 @@ import {AuthContext} from "../context/AuthProvider";
 import axios from "axios";
 import VistaError from "../components/UI/VistaError";
 import {errorRandomToIError, IError} from "./ErrorModel";
-import {IProducto, PRODUCTO_TIPO_COMBO, productoVacio, URL_GET_PRODUCTOS} from "./Producto";
+import {ColumnType} from "antd/lib/table/interface";
+import {Button, Input, Space} from "antd";
+import {SearchOutlined} from "@ant-design/icons";
+import Highlighter from "react-highlight-words";
+
+export type ColumnTipoModel<M> = ColumnType<M> & {
+    dataIndex: keyof M
+}
+
+export type ColumnasTipoModel<M> = ColumnTipoModel<M>[]
+
+export type LaravelBoolean = "0" | "1"
 
 export type ItemSorteado<T extends string> = {
     code: T,
@@ -17,7 +28,9 @@ export interface ItemBusqueda<T> {
     valor: string|number|string[]|number[]
 }
 
-export interface Ideable {
+export type Modelable = Record<string,any>
+
+export interface Ideable extends Modelable{
     id?: number|null
 }
 
@@ -29,6 +42,113 @@ type Editor<T> = {
     (url:string, nuevo?:T, original?:T): Promise<T|undefined>
 }
 
+/**
+ * Retorna true si son iguales en base a los keys
+ * @param arr1
+ * @param arr2
+ * @param keys
+ * comparador debe retornar true si son iguales
+ */
+export function compararArray<T>(arr1:T[], arr2:T[], keys?: ({ key: keyof T, comparador?: {(i1: any, i2:any):boolean} })[]): boolean {
+    return arr1.length === arr2.length
+        && arr1.every(a1 => arr2.find(a2 => keys ?
+            keys.every(k=> k.comparador? k.comparador(a2[k.key],a1[k.key]) : a2[k.key] === a1[k.key])
+            : a2 === a1)
+        )
+}
+
+/**
+ * Recibe un item y un array de items y determina si el item se encuentra dentro del array
+ * @param item
+ * @param arr
+ * @param keys
+ * comparador debe retornar true si los dos items son iguales
+ */
+export function existeItemEnArray<T>(item: T, arr: T[], keys?: ({ key: keyof T, comparador?: {(i1: any, i2:any):boolean} })[]): boolean {
+    return 0<=arr.findIndex(i=> keys ?
+        keys.every(k => k.comparador ? k.comparador(i[k.key], item[k.key]) : i[k.key] === item[k.key])
+        : i === item
+    )
+}
+
+export function createColumnItemFromKey<M extends Modelable>(k: keyof M, titulo?: string, sortOrder?: SortOrder, sortable?: boolean, searchable?:boolean): ColumnTipoModel<M> {
+    const key = typeof k === 'number' ? k : k as string
+    const keyString = k as string
+    const title = titulo || ((k as string).charAt(0).toUpperCase() + (k as string).slice(1))
+    const searchInput = undefined
+    const handleSearch = (...a:any) => {}
+    const setSearchText = (...a:any) => {}
+    const setSearchedColumn = (...a:any) => {}
+    const searchText = ''
+    return {
+        dataIndex: key,
+        key,
+        title,
+        sortOrder: sortOrder,
+        sorter: sortable?{
+            multiple: 1
+        }: undefined,
+        filterDropdown: searchable ? ({ setSelectedKeys, selectedKeys, confirm}) => (
+            <div style={{ padding: 8 }}>
+                <Input
+                    ref={searchInput}
+                    placeholder={`Search ${keyString}`}
+                    value={selectedKeys[0]}
+                    onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={(e) => {
+                        e.stopPropagation()
+                        handleSearch(selectedKeys as string[], confirm, keyString)
+                    }}
+                    style={{ marginBottom: 8, display: 'block' }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => handleSearch(selectedKeys as string[], confirm, keyString)}
+                        icon={<SearchOutlined />}
+                        size="small"
+                        style={{ width: 90 }}
+                    >
+                        Search
+                    </Button>
+                    <Button
+                        onClick={() => {
+                            setSelectedKeys([''])
+                            handleSearch(selectedKeys as string[], confirm, keyString)
+                            // clearFilters && handleReset(clearFilters)
+                        }}
+                        size="small"
+                        style={{ width: 90 }}
+                    >
+                        Reset
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            confirm({ closeDropdown: false });
+                            setSearchText((selectedKeys as string[])[0]);
+                            setSearchedColumn(keyString);
+                        }}
+                    >
+                        Filter
+                    </Button>
+                </Space>
+            </div>
+        ) : undefined,
+        filterIcon: searchable ? (filtered: boolean) => (
+            <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
+        ) : undefined,
+        render: searchable ? text => (
+                <Highlighter
+                    highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+                    searchWords={[searchText]}
+                    autoEscape
+                    textToHighlight={text ? text.toString() : ''}
+                />
+            ) : undefined,
+    }
+}
 
 export const useGenericModel = <Model extends Ideable, ModelSort extends string, ModelQuery>(url:string, page: number, perPage: number, postable?: Postable<Model>, sortBy?: ItemSorteado<ModelSort>[], itemsBusqueda?: Partial<ModelQuery>) => {
     const editor: undefined|Editor<Model> = useMemo(()=>postable ? ((url1, nuevo, original) => editorModel<Model>(url,nuevo,original,postable)) : undefined,[postable, url])
